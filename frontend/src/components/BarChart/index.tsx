155d9a1d149ef06aa4cce59e76b9d131ffa6d94b
LucasAnitelli <luketa2anitelli@hotmail.com>
2021-05-08 22:43:25 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,36 +1,82 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSucess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
 
 const BarChart = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
 
-    const options = {
-        plotOptions: {
-            bar: {
-                horizontal: true,
-            }
-        },
-    };
-    
-    const mockData = {
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/sucess-by-seller`).then((response) => {
+      const data = response.data as SaleSucess[];
+      const myLabels = data.map((item) => item.sellerName);
+      const mySeries = data.map((item) =>
+        round((100.0 * item.deals) / item.visited, 1)
+      );
+
+      setChartData({
         labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+          categories: myLabels,
         },
         series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-            }
-        ]
-    };
+          {
+            name: "% Sucess",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
 
-    return (
-      <Chart 
-        options={{...options, xaxis: mockData.labels}}
-        series={mockData.series}
-        type="bar"
-        height="240"
-      />
-    );
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
   };
-  
-  export default BarChart;
-  
+
+  return (
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
+      type="bar"
+      height="240"
+    />
+  );
+};
+
+export default BarChart;
+
+function series<T>(
+  labels: any,
+  arg1: { categories: never[] },
+  series: any,
+  arg3: { name: string; data: never[] }[]
+): [any, any] {
+  throw new Error("Function not implemented.");
+}
